e990338b3cc15cbaabed9d408e4dabbb46949452
zhangsha <zhangsha@qianhaitun.com>
2022-10-17 18:01:31 +0800

    tp

---
+++ app/controller/Chat.php
@@ -0,0 +1,96 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\controller;
+
+use think\Request;
+use GatewayClient\Gateway;
+
+class Chat
+{
+    public function initialize()
+    {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: token,random,Origin, X-Requested-With, Content-Type, Accept");
+        header('Access-Control-Allow-Methods: POST,GET');
+        if(request()->isOptions()){
+            exit();
+        }
+        Gateway::$registerAddress = "127.0.0.1:1236";
+    }
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param  \think\Request  $request
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        //
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param  \think\Request  $request
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param  int  $id
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+}
---
+++ app/service/Events.php
@@ -0,0 +1,91 @@
+<?php
+namespace app\service;
+use GatewayWorker\Lib\Gateway;
+use Workerman\Worker;
+use think\worker\Application; // 特别注意：下方用到的Application属于think\work命名空间下的类，由于命名空间的更换，需要这样引入才能使用，否则会报错找不到这个类。
+ 
+/**
+ * Worker 命令行服务类
+ */
+class Events
+{
+    /**
+     * onWorkerStart 事件回调
+     * 当businessWorker进程启动时触发。每个进程生命周期内都只会触发一次
+     *
+     * @access public
+     * @param  \Workerman\Worker    $businessWorker
+     * @return void
+     */
+    public static function onWorkerStart(Worker $businessWorker)
+    {
+        $app = new Application;
+        $app->initialize();
+    }
+ 
+    /**
+     * onConnect 事件回调
+     * 当客户端连接上gateway进程时(TCP三次握手完毕时)触发
+     *
+     * @access public
+     * @param  int       $client_id
+     * @return void
+     */
+    public static function onConnect($client_id)
+    {
+        Gateway::sendToCurrentClient("Your client_id is $client_id");
+    }
+ 
+    /**
+     * onWebSocketConnect 事件回调
+     * 当客户端连接上gateway完成websocket握手时触发
+     *
+     * @param  integer  $client_id 断开连接的客户端client_id
+     * @param  mixed    $data
+     * @return void
+     */
+    public static function onWebSocketConnect($client_id, $data)
+    {
+        var_export($data);
+        if (!isset($data['get']['token'])) {
+            Gateway::closeClient($client_id);
+        }
+    }
+ 
+    /**
+     * onMessage 事件回调
+     * 当客户端发来数据(Gateway进程收到数据)后触发
+     *
+     * @access public
+     * @param  int       $client_id
+     * @param  mixed     $data
+     * @return void
+     */
+    public static function onMessage($client_id, $data)
+    {
+        Gateway::sendToAll($data);
+    }
+ 
+    /**
+     * onClose 事件回调 当用户断开连接时触发的方法
+     *
+     * @param  integer $client_id 断开连接的客户端client_id
+     * @return void
+     */
+    public static function onClose($client_id)
+    {
+        GateWay::sendToAll("client[$client_id] logout\n");
+    }
+ 
+    /**
+     * onWorkerStop 事件回调
+     * 当businessWorker进程退出时触发。每个进程生命周期内都只会触发一次。
+     *
+     * @param  \Workerman\Worker    $businessWorker
+     * @return void
+     */
+    public static function onWorkerStop(Worker $businessWorker)
+    {
+        echo "WorkerStop\n";
+    }
+}
